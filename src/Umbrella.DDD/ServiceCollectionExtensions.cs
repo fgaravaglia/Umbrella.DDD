8af403976b2da93bd0ca4e8fa77e80dd89437b98
Francesco Garavaglia <Francesco.Garavaglia@outlook.it>
2022-12-24 21:26:46 +0100

    released version 1.0 in netstd and net6

--- src/Umbrella.DDD/ServiceCollectionExtensions.cs
+++ src/Umbrella.DDD/ServiceCollectionExtensions.cs
@@ -83,7 +83,8 @@ namespace Umbrella.DDD
             foreach(var x in resolvers)
             {
                 IMessageBusDependencyResolver res = (IMessageBusDependencyResolver)Activator.CreateInstance(x);
-                services.AddEventHandlers(res);
+                if(res != null)
+                    services.AddEventHandlers(res);
             }
         }
         /// <summary>
@@ -109,71 +110,6 @@ namespace Umbrella.DDD
                 throw new NullReferenceException($"Unable to load {assemblyName}");
 
             services.AddEventHandlers(targetAssembly);
-        }
-
-
-        /// <summary>
-        /// Scans the assembly and instances all classes derivating from <see cref="IDependencyResolver"/> to load <see cref="ISaga"/> stored in such assembly.
-        /// </summary>
-        /// <remarks>It assumes that the dependency resolver concrete class has a no-parameter constructor</remarks>
-        /// <param name="services"></param>
-        /// <param name="assemblyFolder"></param>
-        /// <param name="assemblyName"></param>
-        /// <param name="addMessagehandlers">true to inject IMessagehandlers</param>
-        /// <param name="addLongRunningProcesses">true to inject sagas</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="NullReferenceException"></exception>
-        [Obsolete("To be removed in next major release. use AddEventHandlers instead")]
-        public static void AddEventHandlersAndLRP(this IServiceCollection services, string assemblyFolder, string assemblyName,
-                                                bool addMessagehandlers = true, bool addLongRunningProcesses = true)
-
-        {
-            if (services == null)
-                throw new ArgumentNullException(nameof(services));
-            if (string.IsNullOrEmpty(assemblyFolder))
-                throw new ArgumentNullException(nameof(assemblyFolder));
-            if (string.IsNullOrEmpty(assemblyName))
-                throw new ArgumentNullException(nameof(assemblyName));
-
-            // get assembly
-            var assemblyPath = Path.Combine(assemblyFolder, assemblyName);
-            var targetAssembly = Assembly.Load(assemblyPath);
-            if (targetAssembly == null)
-                throw new NullReferenceException($"Unable to load {assemblyName}");
-
-            services.AddEventHandlersAndLRP(targetAssembly, addMessagehandlers: false, addLongRunningProcesses: true);
-        }
-        /// <summary>
-        /// Scans the assembly and instances all classes derivating from <see cref="IDependencyResolver"/> to load <see cref="ISaga"/> and <see cref="IMessageHandler{T}"/>  stored in such assembly.
-        /// </summary>
-        /// <remarks>It assumes that the dependency resolver concrete class has a no-parameter constructor</remarks>
-        /// <param name="services"></param>
-        /// <param name="targetAssembly"></param>
-        /// <param name="addMessagehandlers"></param>
-        /// <param name="addLongRunningProcesses"></param>
-        /// <exception cref="ArgumentNullException"></exception>
-        [Obsolete("To be removed in next major release. use AddEventHandlers instead")]
-        public static void AddEventHandlersAndLRP(this IServiceCollection services, Assembly targetAssembly, bool addMessagehandlers = true, bool addLongRunningProcesses = true)
-        {
-            if (services == null)
-                throw new ArgumentNullException(nameof(services));
-            if (targetAssembly == null)
-                throw new ArgumentNullException(nameof(targetAssembly));
-
-            // extract all Ihandler<> 
-            var resolvers = targetAssembly.GetTypes().Where(x =>
-            {
-                return x.GetInterfaces().Any(i => i.IsAssignableFrom(typeof(IMessageBusDependencyResolver)));
-            }).ToList();
-            // instance all types and fill services
-            foreach (var resolverType in resolvers)
-            {
-                IMessageBusDependencyResolver res = (IMessageBusDependencyResolver)Activator.CreateInstance(resolverType);
-                if(addMessagehandlers)
-                    res.AddEventHandlers(services);
-                if(addLongRunningProcesses)
-                    res.AddSagas(services);
-            }
         }
     }
 }
