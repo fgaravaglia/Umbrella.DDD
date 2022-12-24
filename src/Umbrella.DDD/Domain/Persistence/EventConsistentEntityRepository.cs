264f6ce5569770a42c4221e3ba60ba89713ca6ef
Francesco Garavaglia <Francesco.Garavaglia@outlook.it>
2022-12-24 12:26:49 +0100

    added repository to keep consistency of events

---
+++ src/Umbrella.DDD/Domain/Persistence/EventConsistentEntityRepository.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Umbrella.DDD.Abstractions;
+using Umbrella.DDD.Abstractions.Domains;
+
+namespace Umbrella.DDD.Domain.Persistence
+{
+    /// <summary>
+    /// Base implementation of repository that keeps event consistency of the entity
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="Tdto"></typeparam>
+    public class EventConsistentEntityRepository<T, Tdto> : IEntityRepository<T>
+        where T : IEntity
+        where Tdto : class
+    {
+        #region protected methods
+        readonly protected IEntityRepository<T> _Repository;
+        readonly protected IMessageBus _Bus;
+        readonly protected ILogger _Logger;
+        #endregion
+
+        /// <summary>
+        /// Default constr
+        /// </summary>
+        /// <param name="repo"></param>
+        protected EventConsistentEntityRepository(ILogger logger, IEntityRepository<T> repo, IMessageBus bus)
+        {
+            this._Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this._Repository = repo ?? throw new ArgumentNullException(nameof(repo));
+            this._Bus = bus ?? throw new ArgumentNullException(nameof(bus));
+        }
+
+        /// <summary>
+        /// Gets the entity from persistence
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public T? Get(string id)
+        {
+            return this._Repository.Get(id);
+        }
+        /// <summary>
+        /// Saves the entity. Since this repository keeps the event consistency, all uncommitted changes are publishes using MessageBus
+        /// </summary>
+        /// <param name="entity"></param>
+        public void Save(T entity)
+        {
+           this._Repository.Save(entity);
+
+           // gets the uncommitted events
+           var events = entity.GetUncommittedChanges();
+           foreach(var msg in events)
+           {
+                try
+                {
+                    this._Bus.PublishEvent(msg);
+                }
+                catch(Exception ex)
+                {
+                    this._Logger.LogError(ex);
+                }
+           }
+        }
+    }
+}
